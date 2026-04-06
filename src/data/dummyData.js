76ebd2472f8d9d969f2e606c9fa5638d45ee1e11
Sourabh Sharma <sourabhsharma0806@gmail.com>
2026-04-06 20:37:45 +0530

    feat: add mock knowledge base data

---
+++ src/data/dummyData.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+  {
+    title: "Test",
+    description: "Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has been the industry's standard dummy.",
+    date: "14/07/2025",
+  },
+];
+
+export default data;
